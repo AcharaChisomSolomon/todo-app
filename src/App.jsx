e874e278983c46dc0ee0dd5ce6b434cec5a7ac36
AcharaChisomSolomon <acharachisom861@gmail.com>
2025-07-23 15:22:01 +0100

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+function App() {
+
+  return <Button>This my button component.</Button>
+}
+
+const Button = styled.button`
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`
+
+export default App
